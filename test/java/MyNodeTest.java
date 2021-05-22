6410e01a3ea05b246c9b49cfa0b33e33a4d523b5
snehalupatil <patilsnehal4575@gmail.com>
2021-05-22 11:03:41 +0530

    Added: Create a simple Linked List

---
+++ test/java/MyNodeTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MyNodeTest {
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
+
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer>  myThirdNode = new MyNode<>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                         mySecondNode.getNext().equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
+
+
+}
